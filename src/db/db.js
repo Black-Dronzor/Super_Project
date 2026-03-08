fc62c2f88c64fe1c7b5526ade888fa79c5b59008
Umar Farooq <dronzor@gearup.com>
2026-03-08 12:56:48 +0530

    part-8

--- src/db/db.js
+++ src/db/db.js
@@ -5,10 +5,7 @@ const connectDB = async() => {
     try {
        const ConnectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
        console.log(`\nMONGODB connected!! DB host : ${ConnectionInstance.connection.host}`)
-    //    console.log("HI how are you")
-    //    app.listen(process.env.PORT, () => {
-    //     console.log("App is listening on port", process.env.PORT)
-    //    })
+   
     } catch (error) {
         console.log("MONGODB CONNECTION FAILED", error);
         process.exit(1);
---
+++ src/utils/ApiError.js
@@ -0,0 +1,27 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message = "something went wrong",
+        errors = [], 
+        stack = "",
+
+    ) {
+        super(message)
+        this.message = message
+        this.data = null
+        this.statusCode = statusCode
+        this.success = false
+        this.errors = errors 
+    
+        if (stack) {
+        this.stack = stack
+
+        } else {
+        Error.CaptureStackTrace(this, this.constructor)
+
+        }
+    }
+
+}
+
+export {ApiError}
